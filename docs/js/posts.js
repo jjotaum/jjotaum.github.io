2c3bf628771e6bbfbcbd901b69a600635123e3c1
Jota Uribe <jjotaum@gmail.com>
2026-03-23 16:29:07 -0500

    Add new app Bolt, Add Bolt blog entry

--- docs/js/posts.js
+++ docs/js/posts.js
@@ -134,6 +134,7 @@ document.addEventListener("DOMContentLoaded", () => {
   function formatDate(dateString) {
     const date = new Date(dateString);
     return date.toLocaleDateString("en-US", {
+      timeZone: "UTC",
       year: "numeric",
       month: "long",
       day: "numeric",
